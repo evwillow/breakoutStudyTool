21996a7de4b03f4bff3299de44dfca0f02425a42
evan <evanmaus1@gmail.com>
2025-11-05 04:02:02 -0800

    fix

--- src/web/components/DateFolderBrowser/DateFolderBrowser.d.ts
+++ src/web/components/DateFolderBrowser/DateFolderBrowser.d.ts
@@ -5,9 +5,9 @@ import { FlashcardData } from '../Flashcards/utils/dataProcessors';
 export interface DateFolderBrowserProps {
   session: Session | null;
   currentStock: string | null;
-  isTimeUp: boolean;
   flashcards?: FlashcardData[];
   currentFlashcard?: FlashcardData | null;
+  onChartExpanded?: () => void;
 }
 
 declare const DateFolderBrowser: React.FC<DateFolderBrowserProps>;
